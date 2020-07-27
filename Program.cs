7af19cddb90ae9c08d293c0987920013319838e7
iuri-pdista <correaiuri86@gmail.com>
2020-07-26 23:57:08 -0300

    Adcionei interface

--- Program.cs
+++ Program.cs
@@ -4,9 +4,40 @@ using _USERS;
 
 namespace Programming{
     class Program{
-       
+        private static void ClienteFunc(){
+            Cliente cliente = new Cliente();
+            cliente.SalvarCliente(); 
+            cliente.GravarCliente();  
+        }
+        private static void UserFunc(){
+            User User = new User();
+            User.SalvarUser(); 
+            User.GravarUser();  
+        }
+        private static void Menu(){
+            string MenuLog = "\nBem Vindo ao sistema gerenciador de informações"+ 
+            "\n\n Pressione >0< para SAIR" + 
+            "\nPressione >1< para CLIENTES" + 
+            "\nPressione >2< Para FUNCIONÁRIOS" + 
+            "\n Pressione >3< Para produtos";
+            while(true){
+                Console.WriteLine(MenuLog);
+                string MenuRes = Console.ReadLine();
+                if (MenuRes == "0"){break;}
+                else if (MenuRes == "1"){
+                    ClienteFunc();
+                }
+                else if (MenuRes == "2"){
+                    UserFunc();
+                }
+                else if (MenuRes == "3"){
+                    Console.WriteLine("Essa funcionalidade ainda não foi implementada");
+                    break;
+                }
+            }
+        } 
         static void Main(){
-            
+            Menu();
         }
     }
 }
