73c685c26058cb60d2ee0b0701aad5f1848b0c0b
iuri-pdista <correaiuri86@gmail.com>
2020-07-27 00:17:17 -0300

    Import do namespace de classe de produto

--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using _CLIENTE;
 using _USERS;
+using _PRODUTOS;
 
 namespace Programming{
     class Program{
@@ -13,6 +14,12 @@ namespace Programming{
             User User = new User();
             User.SalvarUser(); 
             User.GravarUser();  
+        }
+
+        private static void ProdutoFunc(){
+            Produto produto = new Produto();
+            produto.SalvarProduto(); 
+            produto.GravarProduto();
         }
         private static void Menu(){
             string MenuLog = "\nBem Vindo ao sistema gerenciador de informações"+ 
