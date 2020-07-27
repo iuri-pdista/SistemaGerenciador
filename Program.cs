f6eee9c89a4e2115796c66ec02e4778fe80d21c0
iuri-pdista <correaiuri86@gmail.com>
2020-07-26 23:56:19 -0300

    Adcionei os imports de classe

--- Program.cs
+++ Program.cs
@@ -1,7 +1,12 @@
-﻿namespace Programming{
+﻿using System;
+using _CLIENTE;
+using _USERS;
+
+namespace Programming{
     class Program{
        
         static void Main(){
+            
         }
     }
 }
