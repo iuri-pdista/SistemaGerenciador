1cf0fae3c67e4d83e39af99624922ae61b1b9d97
iuri-pdista <correaiuri86@gmail.com>
2020-07-27 00:29:53 -0300

    Melhorei o menu :lipsticl:

--- Program.cs
+++ Program.cs
@@ -11,9 +11,28 @@ namespace Programming{
             cliente.GravarCliente();  
         }
         private static void UserFunc(){
-            User User = new User();
-            User.SalvarUser(); 
-            User.GravarUser();  
+            while(true){
+                string MenuLog = "\nO que deseja fazer na secção de funcionários?"+ 
+                "\n\nPressione >0< para VOLTAR AO MENU"+ 
+                "\nPressione >1< para ADICIONAR NOVOS funcionários"+ 
+                "\nPressione >2< Para EDITAR um funcionário"+ 
+                "\nPressione >3< Para LISTAR TODOS os funcionários\n\n";
+                Console.WriteLine(MenuLog);
+                string MenuRes = Console.ReadLine();
+                if (MenuRes == "0"){break;}
+                else if(MenuRes == "1"){
+                    User User = new User();
+                    User.SalvarUser(); 
+                    User.GravarUser();
+                }
+                else if(MenuRes == "2"){
+                    Console.WriteLine("Essa funcionalidade ainda nao foi implementada");
+                }
+                else if(MenuRes == "3"){
+                    
+                }
+                  
+            }
         }
 
         private static void ProdutoFunc(){
@@ -38,8 +57,7 @@ namespace Programming{
                     UserFunc();
                 }
                 else if (MenuRes == "3"){
-                    Console.WriteLine("Essa funcionalidade ainda não foi implementada");
-                    break;
+                    ProdutoFunc();
                 }
             }
         } 
